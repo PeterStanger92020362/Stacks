3e47eeb1c372747bf965f7783d23e5e7bc653119
PeterStanger92020362 <psta191@mywhitecliffe.com>
2020-06-11 21:16:17 +1200

    Initial Commit

---
+++ src/stacks/Stacks.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stacks;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Peter
+ */
+public class Stacks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        /*
+        Stack plates = new Stack();      
+ 
+        for (int i = 1; i <= 10; i++) {
+            plates.push(i);
+        }
+        
+        System.out.println(plates);
+        
+        while ( !plates.empty()){
+            System.out.println("\nThe next value is: " + plates.peek());
+            System.out.print("The value removed is: " + plates.pop());
+            
+            System.out.print(",");
+        }
+        System.out.println("\nLIFT-OFF!!!!");
+        System.out.println("\nList is empty?: " + plates.isEmpty());
+        */
+        
+        
+        
+    }
+    
+}
